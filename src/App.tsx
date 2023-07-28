57150f6535a8ba921cee2d08f2c14e255fd5e3a3
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-07-28 14:15:15 -0300

    all libs were added

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default App;
